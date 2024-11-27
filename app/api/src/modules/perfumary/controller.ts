eed06fd3ab56e0ea170e4e5a72ffcea4f6c86b72
Bruno <bruno.ramos@ccc.ufcg.edu.br>
2024-11-27 18:12:53 -0300

    feat: Implementação do controller Perfumery

--- app/api/src/modules/perfumary/controller.ts
+++ app/api/src/modules/perfumary/controller.ts
@@ -1,6 +1,7 @@
 import { Perfumery } from "@prisma/client";
-import { createPerfumery, getAllPerfumeries } from "./service";
+import { addParfumToPerfumery, createPerfumery, deletePerfumery, getAllPerfumeries, getPerfumeryByName, putPerfumery } from "./service";
 import { Request, Response } from "express";
+import { HttpCodes } from './../../utils/httpCodes';
 
 type PerfumeryInput = Omit<Perfumery, "id" >;
 
@@ -8,11 +9,40 @@ export async function handleCreate(request: Request, response: Response): Promis
     const { name }: PerfumeryInput = request.body;
     const perfumery = await createPerfumery({ name });
     
-    return response.status(201).json(perfumery); 
+    return response.status(HttpCodes.CREATED).json(perfumery); 
 }
 
 export async function handleGetAllPerfumeries(request: Request, response: Response): Promise<Response> {
     const perfumeries = await getAllPerfumeries();
 
-    return response.status(200).json(perfumeries);
+    return response.status(HttpCodes.OK).json(perfumeries);
+}
+
+export async function handleGetPerfumeryByName(request: Request, response: Response): Promise<Response> {
+    const { name }: PerfumeryInput = request.body;
+    const matchedPerfumeries = await getPerfumeryByName(name);
+
+    return response.status(HttpCodes.OK).json(matchedPerfumeries);
+}
+
+export async function handlePutPerfumery(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const data: PerfumeryInput = request.body;
+    const updatedPerfumery = await putPerfumery(id, data);
+
+    return response.status(HttpCodes.OK).json(updatedPerfumery);
+}
+
+export async function handleDeletePerfumery(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const deletedPerfumery = await deletePerfumery(id);
+
+    return response.status(HttpCodes.NO_CONTENT).json(deletedPerfumery);
+}
+
+export async function handleAddParfumToPerfumery(request: Request, response: Response) {
+    const { perfumeryId, parfumId } = request.params;
+    const perfumery = await addParfumToPerfumery(perfumeryId, parfumId);
+
+    return response.status(HttpCodes.OK).json(perfumery);
 }
---
+++ app/api/src/utils/httpCodes.ts
@@ -0,0 +1,7 @@
+export const HttpCodes = {
+    CREATED: 201,
+    OK: 200,
+    NO_CONTENT: 204,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500
+}
